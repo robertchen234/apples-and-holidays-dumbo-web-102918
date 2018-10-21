0460cf00e6d9822c0db1c4c4c4ea5109291a6111
Robert Chen <robertchen234@gmail.com>
2018-10-21 17:12:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,12 +37,10 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash[:spring][:memorial_day] << "Grill"
   holiday_hash[:spring][:memorial_day] << "Table Cloth"
 end
-end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash[:spring][:memorial_day] << "Table Cloth"
 end
 
 def all_winter_holiday_supplies(holiday_hash)
