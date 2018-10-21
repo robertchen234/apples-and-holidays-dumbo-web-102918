80f990f5456d2826018cd9fcc43970de44eba94b
Robert Chen <robertchen234@gmail.com>
2018-10-21 17:26:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season.capitalize
+    puts season.to_s.capitalize
     puts "  #{holiday}.split.capitalize!.join(" ")"
   end 
 end
