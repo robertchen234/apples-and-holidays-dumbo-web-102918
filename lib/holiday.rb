4ffb5f4b973ba11fb79e4ee7766d9f3a53a78275
Robert Chen <robertchen234@gmail.com>
2018-10-21 18:09:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_holidays_with_bbq(holiday_hash)
   bbq = []
   holiday_hash.each do | season, holidays |
     holidays.each do | holiday, supply |
-      supply == "BBQ" ? bbq << holiday
+      supply == "BBQ" ? bbq << holiday : nil
     end
   end 
   bbq
