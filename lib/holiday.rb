9cc6fa6cf5f024b281095348f46ce477da1fce6a
Robert Chen <robertchen234@gmail.com>
2018-10-21 17:29:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
-    puts "  #{holiday.split.to_s.capitalize.join(' ')}"
+    puts "  #{holiday}""
   end 
 end
 
