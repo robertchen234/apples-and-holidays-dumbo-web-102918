f6d262958c2798997471130a1ade1a88460c0916
Robert Chen <robertchen234@gmail.com>
2018-10-21 18:07:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do | season, holidays |
-    holidays.select do | supply |
+    holidays.find_all do | supply |
       supply == "BBQ"
     end
   end 
